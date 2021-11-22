a8e30c379189efca01871f18cb44b6c5a55d19d5
ghkim23 <ghkim1688@gmail.com>
2021-11-22 14:20:26 +0900

    프로젝트 생성

---
+++ src/test/java/com/gh/tradebot/TradebotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gh.tradebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradebotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
